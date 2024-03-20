df81ce9a6a510bc80218be8829f1182b1ede78e2
NarutoSasukeForever <felzina_liza@mail.ru>
2024-03-20 22:42:26 +0300

    commit3

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -12,9 +12,9 @@ const Modal = ({ isOpen, onClose }) => {
           <img src="./icon.png" alt="icon" />
         </button>
         <div className={styles.content}>
-          <h2 className="text text_type_digits-large">034536</h2>
+          <p className={styles.digits}>034536</p>
           <p className="text text_type_main-small">индентификатор заказа</p>
-            <img src="./done.png" alt="done"/>
+            <img src="./done.png" alt="done" className={styles.image}/>
           <p className="text text_type_main-small mb-2">Ваш заказ начали готовить</p>
           <p className="text text_type_main-small text_color_inactive">Дождитесь готовности на орбитальной станции</p>
         </div>
