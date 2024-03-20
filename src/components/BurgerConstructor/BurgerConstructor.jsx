1aaf6100780c1cffe8541845fe6ba2dd82278af5
NarutoSasukeForever <felzina_liza@mail.ru>
2024-03-20 22:31:44 +0300

    commit-with-modal

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -3,11 +3,19 @@ import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-comp
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components'
 import { DragIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import Modal from '../Modal/Modal';
+import React, { useState } from 'react';
 
 const BurgerConstructor = ({ingredients}) => {
   const topElements = ingredients.filter(item => item.name === "Краторная булка N-200i");
   const bottomElements = ingredients.filter(item => item.name === "Краторная булка N-200i");
   const mainElements = ingredients.filter(item => item.type === 'main');
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOrderClick = () => {
+    setIsModalOpen(true); 
+  };
 
   return (
     <section className={styles.container}>
@@ -56,9 +64,10 @@ const BurgerConstructor = ({ingredients}) => {
       
         <div className={styles.buttonContainer}>
           <div className={styles.priceBottom}><p className="text text_type_digits-medium mr-1">610</p><CurrencyIcon type="primary" /></div>
-          <Button htmlType="button" type="primary" size="large">
+          <Button htmlType="button" type="primary" size="large" onClick={handleOrderClick}>
           Оформить заказ
           </Button>
+          <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
         </div>
 </section>
   )
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './ModalOverlay.module.css';
 
-const Modal = ({ isOpen, onClose, children }) => {
+const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
@@ -11,7 +12,11 @@ const Modal = ({ isOpen, onClose, children }) => {
           <img src="./icon.png" alt="icon" />
         </button>
         <div className={styles.content}>
-          <p>Hello</p>
+          <h2 className="text text_type_digits-large">034536</h2>
+          <p className="text text_type_main-small">индентификатор заказа</p>
+            <img src="./done.png" alt="done"/>
+          <p className="text text_type_main-small mb-2">Ваш заказ начали готовить</p>
+          <p className="text text_type_main-small text_color_inactive">Дождитесь готовности на орбитальной станции</p>
         </div>
       </div>
     </div>,
